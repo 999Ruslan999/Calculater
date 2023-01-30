ebcff3f7b0c300a9b84c0e1b2720d1ac6b09d5a7
999Ruslan999 <113248592+999Ruslan999@users.noreply.github.com>
2023-01-30 18:27:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.zeroerr.apply(a, b);
+
+        calc.println.accept(c);
+    }
+}
